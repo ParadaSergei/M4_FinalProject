3c1bfd5ba32c03a20cc1475e154db18bbac0c87c
DES557 <153308905+DES557@users.noreply.github.com>
2024-04-17 23:45:47 +0300

    Начал Разработку AIEnemy
    Подготовил NavMesh Agent для дальнейших экспериментов

---
+++ Assets/Mironov/Scripts/AI Enemy/AIEnemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIEnemy : MonoBehaviour
+{
+    public List<Transform> PatrolPoints;
+    public Transform Player;
+    public float Damage = 30;
+
+    private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+
+        CheckPlayer();
+        if (CheckPlayer())
+        {
+            _navMeshAgent.SetDestination(Player.position);
+        }
+        else
+        {
+            Patrol();
+        }
+        AttackUpdate();
+    }
+
+    bool CheckPlayer()
+    {
+        var direction = Player.position - transform.position;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            if (hit.transform == Player)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void Patrol()
+    {
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            _navMeshAgent.SetDestination(PatrolPoints[Random.Range(0, PatrolPoints.Count)].position);
+        }
+    }
+    void AttackUpdate()
+    {
+        if (CheckPlayer())
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                //Player.GetComponent<PlayerHP>().DealDamage(Damage * Time.deltaTime);
+            }
+        }
+    }
+}
