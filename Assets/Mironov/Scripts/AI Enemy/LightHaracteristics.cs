c5a8ac9b59bc165e4bf80c338deff831619f0572
DES557 <153308905+DES557@users.noreply.github.com>
2024-05-06 22:35:15 +0300

    AI Готов
    Наконец то я его починил, я спать)

--- Assets/Mironov/Scripts/AI Enemy/LightHaracteristics.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LightHaracteristics : MonoBehaviour
-{
-    public bool Light = true;
-    public float ForceLight;
-}
