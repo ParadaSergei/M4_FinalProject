4828646dbae76c921b006b650451259d9268128e
Парада Сергей Сергеевич <sergejparada95@gmail.com>
2024-05-10 16:40:58 +0300

    двериDemo

---
+++ Assets/Obchee/Scripts/OpenCloseDoor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class OpenCloseDoor : MonoBehaviour
+{
+    public bool _isOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isOpen = !_isOpen;
+        }
+        if (_isOpen)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -3, transform.position.z), Time.deltaTime/2 );
+        }
+        else if(!_isOpen)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 4, transform.position.z), Time.deltaTime / 2);
+        }
+    }
+}
