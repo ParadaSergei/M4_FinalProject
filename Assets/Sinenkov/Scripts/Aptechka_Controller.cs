29777fb272728cbd9e41fe3f0aec570b9656388c
Picnocliber <illiontar@gmail.com>
2024-05-12 21:57:44 +0300

    что то начал но пока ничего нет

---
+++ Assets/Sinenkov/Scripts/Aptechka_Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "1" && Input.GetKey(KeyCode.Q)) //если объект сталкивается с объектом у которого есть тег "1" и клавиша "Q" нажата то 
+        {
+
+        }
+    }       
+}
