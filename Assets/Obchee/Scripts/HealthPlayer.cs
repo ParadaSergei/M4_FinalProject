46e57c28a6c11eeb2cb702fa40ccccafb51a6c3e
Парада Сергей Сергеевич <sergejparada95@gmail.com>
2024-05-31 01:52:20 +0300

    норм

---
+++ Assets/Obchee/Scripts/HealthPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.GetComponent<HealthBarValue>())
+            {
+                other.GetComponent<HealthBarValue>().TakeHealth(50);
+                Destroy(transform.gameObject);
+            }
+        }
+
+    }
+}
